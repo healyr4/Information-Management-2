2bf3e13f02e5d1ffa8efec93ffc5847167f1d8b5
healyr4 <healyr4@tcd.ie>
2020-11-01 15:05:24 +0000

    Update exercise1.sql

--- Creating-DB/exercise1.sql
+++ Creating-DB/exercise1.sql
@@ -1,3 +1,9 @@
+/*
+1. Create a table called cs_course_modules with the fields module_name (variable char of length 50),
+module_id (variable char of length 10), etc (Number),
+and lecture_hours (Number). 
+All fields must not be null and the module_id must be the primary key
+*/
 CREATE TABLE cs_course_modules (
   module_name VARCHAR(50) NOT NULL,
   module_id VARCHAR(10) NOT NULL,
